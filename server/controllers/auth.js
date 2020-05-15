e5f694ebddba691ac04e778f886ee269eb9a4207
Craig <craigdanj@gmail.com>
2020-05-16 00:34:21 +0530

    password hashing logic added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,34 +1,51 @@
-const { validationResult } = require('express-validator');
+// const { validationResult } = require('express-validator');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.signUp = (req, res, next) => {
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
-
+    
+    //Check if user exists.
     User.findOne({ where: {email} }).then(user => {
         if(user) {
             res.status(500).json({
                 error: "User already exists"
             });
         } else {
-            //Create user
-            User.create({ name, email, password }).then(user => {
-                if(user) {
-                    res.status(200).json({
-                        email: user.name
-                    });
-                } else {
-                    res.status(500).json({
-                        error: "could not create user"
-                    });
-                }
-                
-            }).catch(err => {
-            });
+            //Hash password
+            bcrypt.hash(password, 12).then(hashedPassword => {
 
-            
-        }        
+                //Create user
+                User.create({
+                    name,
+                    email,
+                    password: hashedPassword
+                }).then(user => {
+                    if (user) {
+                        res.status(200).json({
+                            message: `User ${email} created successfully`
+                        });
+                    } else {
+                        res.status(500).json({
+                            error: "Could not create user"
+                        });
+                    }
+                }).catch(err => {
+                    if(!err.statusCode) {
+                        err.statusCode = 500;
+                    }
+                    next(err);
+                });
+
+            }).catch(err => {
+                if(!err.statusCode) {
+                    err.statusCode = 500;
+                }
+                next(err);
+            });
+        }
     });
 };
 
